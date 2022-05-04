0506112770a47179dfda0a2f9abca9c4577c6624
JV <84021274+jylev24@users.noreply.github.com>
2022-05-04 10:10:54 +1000

    Add files via upload

---
+++ frith-backend/secfirm/register.php
@@ -0,0 +1,69 @@
+<?php
+    include("../session.php");
+
+    if($_SESSION["email"] !== "") {
+        header("location:index.php");
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Register | Security Firm</title>
+
+        <!--Google Fonts-->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Domine&family=Nanum+Brush+Script&display=swap" rel="stylesheet">
+
+        <!--Latest compiled and minified CSS-->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <!--jQuery library-->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <!--Bootstrap Icons-->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+        <!--Popper JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+        <!--Latest compiled JavaScript-->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+        <link rel="stylesheet" href="css/style.css">
+
+        <script src="js/register.js"></script>
+    </head>
+
+    <body>
+        <main class="d-flex align-items-center vh-100">
+            <div class="container text-center w-25">
+                <h2 class="font-weight-bold mb-3">Register your security firm</h2>
+                <div class="d-flex justify-content-center text-danger mb-1" id="results"></div>
+                <form id="signupForm">
+                    <div class="form-group">
+                        <input type="text" class="form-control form-control-lg mb-3" name="firmname" placeholder="Security firm name" required>
+                        <input type="text" class="form-control form-control-lg mb-3" name="abn" placeholder="ABN/ACN" required>
+                    </div>
+
+                    <div class="form-row">
+                        <div class="col">
+                            <input type="text" class="form-control form-control-lg mb-3" name="fname" placeholder="Manager first name" required>
+                        </div>
+                        <div class="col">
+                            <input type="text" class="form-control form-control-lg mb-3" name="lname" placeholder="Manager last name" required>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <input type="email" class="form-control form-control-lg mb-3" name="email" placeholder="Email address" required>
+                        <input type="tel" class="form-control form-control-lg mb-3" name="phone" placeholder="Phone number" required>
+
+                        <button type="submit" class="btn btn-lg btn-block btn-success">Register</button>
+                    </div>
+                </form>
+                <p>Already registered? <a href="login.php">Login to your account</a></p>
+            </div>
+        </main>
+    </body>
+</html>

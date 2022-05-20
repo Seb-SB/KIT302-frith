e792e156ee0b6df6ae605cb81baeee37fac598f6
EffyYangFeng <100540527+EffyYangFeng@users.noreply.github.com>
2022-05-20 10:59:47 +1000

    Update security_guards.dart

--- frith-mobile/flutter_application_frith/lib/View/security_guards.dart
+++ frith-mobile/flutter_application_frith/lib/View/security_guards.dart
@@ -16,7 +16,221 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
 
 
+  @overrideimport 'package:fifth_app/ui/View/security_login.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter/src/material/bottom_navigation_bar.dart';
+import 'package:fifth_app/ui/View/phone_page.dart';
+import 'package:fifth_app/ui/notepad/notepad_homepage.dart';
+import 'package:fifth_app/ui/torch/flashlight_page.dart';
+import 'package:url_launcher/url_launcher.dart';
+import '../emergency/emergencyphone.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool light = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Security Guards"),
+          actions: [_popupMenuButton(context)],
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            //crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  //flashlight function
+                  GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          light = !light;
+                        });
+                        // Navigator.push(
+                        //     context,
+                        //     MaterialPageRoute(
+                        //         builder: (context) => const FlashLghtPage()));
+                      },
+                      child: Container(
+                        color: Colors.blueAccent,
+                        width: 150,
+                        height: 100,
+                        child: light
+                            ? Icon(
+                                Icons.lightbulb,
+                                color: Colors.yellow,
+                                size: 80,
+                              )
+                            : Icon(
+                                Icons.lightbulb_outline,
+                                color: Colors.white,
+                                size: 80,
+                              ),
+                      )),
+
+                  GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const NotepadPage(
+                                      title: '',
+                                    )));
+                      },
+                      child: Container(
+                          color: Colors.blueAccent,
+                          width: 150,
+                          height: 100,
+                          child: Icon(
+                            Icons.event_note_rounded,
+                            color: Colors.white,
+                            size: 80,
+                          ))),
+                ],
+              ),
+              SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  //emergency function
+                  GestureDetector(
+                    onTap: () {
+                      launch('tel:000');
+                      // Navigator.push(
+                      //     context,
+                      //     MaterialPageRoute(builder: (context) => const EmergencyPhone(phone: '000',)));
+                    },
+                    child: Container(
+                        //alignment: Alignment.centerLeft,
+                        color: Colors.blueAccent,
+                        width: 150,
+                        height: 100,
+                        child: Icon(
+                          Icons.warning_amber_outlined,
+                          color: Colors.white,
+                          size: 80,
+                        )),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const PhonePage()));
+                    },
+                    child: Container(
+                        // alignment: Alignment.centerRight,
+
+                        width: 150,
+                        height: 100,
+                        color: Colors.blueAccent,
+                        child: Icon(
+                          Icons.time_to_leave_outlined, //request back up
+                          color: Colors.white,
+                          size: 80,
+                        )),
+                  ),
+                ],
+              ),
+              SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                      // alignment: Alignment.centerLeft,
+                      color: Colors.blueAccent,
+                      width: 150,
+                      height: 100,
+                      child: Icon(
+                        Icons.note,
+                        color: Colors.white,
+                        size: 80,
+                      ) //"Incident Report"
+                      ),
+                  Container(
+                      //alignment: Alignment.centerRight,
+
+                      width: 150,
+                      height: 100,
+                      color: Colors.blueAccent,
+                      child: Icon(
+                        Icons.access_time_outlined, //start/end/shift
+                        color: Colors.white,
+                        size: 80,
+                      )),
+                ],
+              ),
+            ],
+          ),
+        ));
+  }
+
+  PopupMenuButton _popupMenuButton(BuildContext context) {
+    return PopupMenuButton(
+      itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+        PopupMenuItem(
+          onTap: () {
+            //
+            print("Logout");
+          },
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              GestureDetector(
+                  onTap: () {
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: Text("Notice"),
+                            content:
+                                Text("Are you sure you would like to Logout?"),
+                            actions: <Widget>[
+                              TextButton(
+                                child: Text("No"),
+                                onPressed: () =>
+                                    Navigator.of(context).pop(), //SecLogin
+                                // onPressed: () {
+                                //   // Navigator.push(
+                                //   //    context,
+                                //   //  MaterialPageRoute(builder: (context) => const (HomePage))); //go back to security login page SecLogin
+                                // },
+                              ),
+                              TextButton(
+                                child: Text("Yes"),
+                                onPressed: () {
+                                  Navigator.pushReplacement(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              SecLogin())); //go back to security login page SecLogin
+                                },
+                              ),
+                            ],
+                          );
+                        });
+                  },
+                  child: new Text('Logout')),
+            ],
+          ),
+          //value: 'logout',
+        ),
+      ],
+    );
+  }
+}
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(

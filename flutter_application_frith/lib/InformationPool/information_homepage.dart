6b6b412fdb7fe16ef2d649c9ecc3e274649e33e0
EffyYangFeng <100540527+EffyYangFeng@users.noreply.github.com>
2022-08-07 08:58:17 +1000

    Update information_homepage.dart
    not working

--- flutter_application_frith/lib/InformationPool/information_homepage.dart
+++ flutter_application_frith/lib/InformationPool/information_homepage.dart
@@ -17,6 +17,61 @@ class _InformationHomepageState extends State<InformationHomepage> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    throw UnimplementedError();
+    return Consumer<InformationPoolModel>(builder: buildScaffold);
+  }
+
+  Scaffold buildScaffold(
+      BuildContext context, InformationPoolModel informationpoolModel, _) {
+    // ignore: dead_code
+    return Scaffold(
+      appBar: AppBar(
+          title: Text('InformationPool'),
+          centerTitle: true),
+
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+             
+              //YOUR UI HERE
+               Expanded(
+                child: ListView.builder(
+                  itemBuilder: (_, index) {
+                    //ignore: non_constant_identifier_names
+                    var informationPool = InformationPoolModel.informationpoolItems[index];
+                    return ListTile(
+                      title: Text(InformationPoll.location),
+                      subtitle: Text(InformationPool.date.toString()),
+                      //leading: Image.network(image),
+
+                      onTap: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) {
+                         // return InformationPollDetails(id: index);
+                        }));
+                      },
+                    );
+                  },
+                  //itemCount: InformationPoolModel.informationpoolItems.length
+                  ),
+            )
+            ],
+          ),
+        ),
+      ),
   }
 }
+//A little helper widget to avoid runtime errors -- we can't just display a Text() by itself if not inside a MaterialApp, so this workaround does the job
+class FullScreenText extends StatelessWidget {
+  final String text;
+
+  const FullScreenText({Key? key, required this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Directionality(
+        textDirection: TextDirection.ltr,
+        child: Column(children: [Expanded(child: Center(child: Text(text)))]));
+  }
+}
+

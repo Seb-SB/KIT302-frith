9793b46608950c34c7793887d84646ab07f7259d
EffyYangFeng <100540527+EffyYangFeng@users.noreply.github.com>
2022-08-07 08:58:52 +1000

    Update informationPool.dart

--- flutter_application_frith/lib/InformationPool/informationPool.dart
+++ flutter_application_frith/lib/InformationPool/informationPool.dart
@@ -3,62 +3,56 @@ import 'information_homepage.dart';
 
 class InformationPool {
   String title;
-  String time;
   String date;
   String details;
 
   InformationPool(
       {required this.title,
-      required this.time,
       required this.date,
       required this.details});
 }
 
 class InformationPoolModel extends ChangeNotifier {
   /// Internal, private state of the list.
-  final List<InformationPool> items = [];
+  final List<InformationPool>informationItems = [];
+
 
   //Normally a model would get from a database here, we are just hardcoding some data for this week
   InformationPoolModel() {
     add(InformationPool(
         title: "Information1",
-        time: "1.00pm",
         date: "2021-05-23 12:00:02.000",
         details: "The first one is ..."));
     add(InformationPool(
         title: "Information2",
-        time: "1.00pm",
         date: "2021-07-23 12:00:02.000",
         details: "The second one is ..."));
     add(InformationPool(
         title: "Information3",
-        time: "1.00pm",
         date: "2021-07-23 12:00:02.000",
         details: "The  note is ..."));
     add(InformationPool(
         title: "Information4",
-        time: "3.00pm",
         date: "2021-05-23 12:00:02.000",
         details: "The  note is ..."));
     add(InformationPool(
         title: "Information5",
-        time: "1.00pm",
         date: "2021-05-23 12:00:02.000",
         details: "The note is ..."));
   }
 
   void add(InformationPool item) {
-    items.add(item);
+    informationItems.add(item);
     update();
   }
 
   void sort() {
-    items.sort(
+    informationItems.sort(
         (a, b) => DateTime.parse(b.date).compareTo(DateTime.parse(a.date)));
   }
 
   void removeAll() {
-    items.clear();
+    informationItems.clear();
     update();
   }
 
@@ -67,4 +61,6 @@ class InformationPoolModel extends ChangeNotifier {
   void update() {
     notifyListeners();
   }
+
+  get(String? id) {}
 }
